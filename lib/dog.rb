3930dacddcac8a9de6f313fc076bfdc0c818cb55
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-08 22:28:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(new_name)
+    @this_dogs_name = new_name
+  end
+  def name
+    @this_dogs_name
+  end
+  def breed=(new_breed)
+    @this_dogs_breed =  new_breed
+  end
+  def breed
+    @this_dogs_breed
+  end
+end
